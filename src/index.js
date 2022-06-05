c4783350add80f0a7619dd120e5440799dc452da
annnacaesar <Anechka22334@gmail.com>
2022-06-06 01:54:08 +0300

    header, hero, description,services,works adaptive

---
+++ src/index.js
@@ -0,0 +1,32 @@
+
+
+// const btnOpen = document.querySelector('.js-open-menu');
+// const btnClose = document.querySelector('.js-close-menu');
+// const menu = document.querySelector('.header__menu');
+
+// btnOpen.addEventListener('click', onOpenMenu);
+// btnClose.addEventListener('click', onCloseMenu);
+
+// function onOpenMenu () {
+// 	menu.classList.add('is-open');
+// }
+
+// function onCloseMenu () {
+// 	menu.classList.remove('is-open');
+// }
+
+// $(document).ready(function () {
+// 	$('.header__burger').click(function (event) {
+		
+// 	})
+// })
+
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+burger.addEventListener('click', onToggle);
+
+function onToggle (arguments) {
+	burger.classList.toggle('active');
+	menu.classList.toggle('active');
+	document.body.classList.toggle('lock');
+}
