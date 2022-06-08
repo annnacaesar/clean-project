610abbc7ab0bb536868cf46c9f5f06ccf7eb9127
annnacaesar <92438869+annnacaesar@users.noreply.github.com>
2022-06-08 09:02:32 +0000

    Deploying to gh-pages from @ annnacaesar/clean-project@7ebc0728c9edcc898ba1e3628a926a5b22170b69 🚀

--- index.dc039e67.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".header__burger"),t=document.querySelector(".header__menu");e.addEventListener("click",(function(){e.classList.toggle("active"),t.classList.toggle("active"),document.body.classList.toggle("lock")}))}();
-//# sourceMappingURL=index.dc039e67.js.map
