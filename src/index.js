7ebc0728c9edcc898ba1e3628a926a5b22170b69
annnacaesar <Anechka22334@gmail.com>
2022-06-08 12:01:36 +0300

    modal

--- src/index.js
+++ src/index.js
@@ -30,3 +30,30 @@ function onToggle () {
 	menu.classList.toggle('active');
 	document.body.classList.toggle('lock');
 }
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop--is-hidden');
+  }
+})();
+
+(() => {
+  document
+    .querySelector('.js-speaker-form')
+    .addEventListener('submit', e => {
+      e.preventDefault();
+
+      new FormData(e.currentTarget).forEach((value, name) =>
+        console.log(`${name}: ${value}`),
+      );
+    });
+})();
